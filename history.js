74d72a2eee2c9e5083d350cfe7ceec15c41f93bb
Eric Stratmann <estrat@cs.stanford.edu>
2010-11-12 16:56:13 -0800

    History page now has a few dummy links

--- history.js
+++ history.js
@@ -1,5 +1,21 @@
 $(init);
 
+function addClickHandlers() {
+    $("#showHistory").click(showHistory);
+    $("#pagesPerDay").click(pagesPerDay);
+}
+
+function showHistory() {
+    $("#results").html("History!");
+    return false;
+}
+
+function pagesPerDay() {
+    $("#results").html("Pages per day!");
+    return false;
+}
+
 function init() {
+    addClickHandlers();
     $("#results").html("History goes here");
 }
